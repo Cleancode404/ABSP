4ea56fc1e1461d7e6f8634c6d3c02776fb6413a1
Heuristic <codemonkey2468@hotmail.com>
2021-02-11 14:04:26 -0500

    update list

--- Chapter4/list.py
+++ Chapter4/list.py
@@ -2,3 +2,16 @@ a = [1, 2, 3, 4, 5, 6]
 
 print(len(a))
 print(a[:4])
+print(a[3])
+print(a[-3])  #negative index (count backward from the right side)
+
+
+
+# animals list
+animals = ['cat', 'dog', 'dog', 'guinea pig', 'dog']
+
+# 'dog' is removed
+animals.remove('dog')
+
+# Updated animals list
+print('Updated animals list: ', animals)
